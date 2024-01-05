eac1dd0445d9f57a88164696b3b00a9da05288d8
David Inoa <davidinoa@gmail.com>
2024-01-05 14:11:19 -0400

    chore: update tailwind config with design system colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      colors: {
+        'copper-canyon': '#D87D4A',
+        'eclipse-black': '#101010',
+        'frost-whisper': '#F1F1F1',
+        'snow-drift': '#FAFAFA',
+        'peachy-sunset': '#FBAF85',
+        'crimson-tide': '#CD2C2C',
+      },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
