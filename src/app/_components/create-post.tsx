00214a3a75ba8aada6d67adebebc7ea0777bf61d
David Inoa <davidinoa@gmail.com>
2024-01-07 17:34:13 -0400

    fix: resolve eslint error

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -27,6 +27,7 @@ export function CreatePost() {
       <input
         type="text"
         placeholder="Title"
+        aria-label="Title"
         value={name}
         onChange={(e) => setName(e.target.value)}
         className="w-full rounded-full px-4 py-2 text-black"
