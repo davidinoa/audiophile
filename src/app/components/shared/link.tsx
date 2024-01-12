5f019425b2be01d272f52b75362585bc73faa9e5
David Inoa <davidinoa@gmail.com>
2024-01-12 14:29:12 -0400

    style(link): add hover effect

--- src/app/components/shared/link.tsx
+++ src/app/components/shared/link.tsx
@@ -1,12 +1,20 @@
 import { Link as NextUiLink } from '@nextui-org/link'
 import NextLink from 'next/link'
+import { mergeClassNames } from '~/lib/utils'
 
 type Props = React.ComponentProps<typeof NextLink>
 
 export default function Link({ href, children, className, ...props }: Props) {
   return (
-    <NextLink href={href} className="leading-none" {...props}>
-      <NextUiLink as="span" tabIndex={-1} className={className}>
+    <NextLink href={href} className="group leading-none" {...props}>
+      <NextUiLink
+        as="span"
+        tabIndex={-1}
+        className={mergeClassNames(
+          'hover:opacity-100 group-hover:text-copper-canyon group-hover:[&_path]:fill-copper-canyon',
+          className,
+        )}
+      >
         {children}
       </NextUiLink>
     </NextLink>
