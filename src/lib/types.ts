b2ca6fcf09196eeb84ec4ef42f8fbad2202f0106
David Inoa <davidinoa@gmail.com>
2024-01-20 03:15:30 -0400

    chore: update image-related types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,30 @@
-export type Category = 'earphones' | 'headphones' | 'speakers'
+import { idToNameMap } from '~/app/[category]/product-data'
+import { CATEGORIES, type BREAKPOINTS } from './constants'
+
+export type Breakpoint = (typeof BREAKPOINTS)[number]
+
+export type Category = (typeof CATEGORIES)[number]
+
+export type ExternalImageData = {
+  width: number
+  height: number
+  src: string
+}
+
+export type ImageSet = Record<Breakpoint, StaticImageData>
+
+type ProductId = keyof typeof idToNameMap
+
+export type ResponsiveImageData = Record<Breakpoint, ExternalImageData>
+
+export type ImageSize = { width: number; height: number }
+
+export type ResponsiveImageSizes = Record<Breakpoint, ImageSize>
+
+export function isProductId(key: string): key is ProductId {
+  return key in idToNameMap
+}
+
+export function isCategory(value: string): value is Category {
+  return CATEGORIES.some((category) => category === value)
+}
