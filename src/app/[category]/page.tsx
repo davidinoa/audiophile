06f145aec783a7d0da89eb7ebc6c4561dff21725
David Inoa <davidinoa@gmail.com>
2024-01-20 03:13:33 -0400

    refactor: remove unused code

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,11 +1,7 @@
 import { notFound } from 'next/navigation'
-import { CATEGORIES } from '~/lib/constants'
-import { type Category } from '~/lib/types'
-import AboutSection from '../components/about-section'
-import CategoryNavigation from '../components/category-navigation'
+import { isCategory } from '~/lib/types'
 import ProductPreview from '../components/product-preview'
 import productsByCategory from './product-data'
-import productImages from './product-images'
 
 type Props = {
   params: {
@@ -18,11 +14,11 @@ export default function Page({ params }: Props) {
   if (!isCategory(category)) return notFound()
 
   return (
-    <main className="content-grid | bg-snow-drift">
+    <>
       <h2 className="full-width bg-eclipse-black p-8 text-center text-[1.75rem] font-bold uppercase leading-snug tracking-[2px] text-white">
         {category}
       </h2>
-      <div className="grid gap-y-28 pt-10 md:pt-20 lg:gap-y-40">
+      <div className="pt-10 md:pt-20">
         <section className="grid gap-28">
           {productsByCategory[category].map((product) => (
             <ProductPreview
@@ -30,20 +26,11 @@ export default function Page({ params }: Props) {
               category={category}
               name={product.name}
               isNew={product.isNew}
-              images={productImages[product.id]}
               description={product.description}
             />
           ))}
         </section>
-        <section>
-          <CategoryNavigation />
-        </section>
-        <AboutSection />
       </div>
-    </main>
+    </>
   )
-}
-
-function isCategory(value: string): value is Category {
-  return CATEGORIES.some((category) => category === value)
 }
